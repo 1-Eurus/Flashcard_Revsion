a98f603717f49d4dca4416c38b5722cc83f5f0f1
dhairya <119152782+dhairyas1@users.noreply.github.com>
2025-08-23 13:24:34 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,8 +1,10 @@
-import pandas as pd 
+
 import random
 
+import csv
 
-file= pd.read_excel("info.xlsx") 
+
+ 
 
 
 class Empty(Exception):
@@ -10,52 +12,65 @@ class Empty(Exception):
         super().__init__(message) #custom exception for empty fields
 
 def exit():
-    global file 
-
+    file= open("info.xlsx",'r', newline='')
+    ro = csv.DictReader(file)
+    overview=[]
+    for i in ro:
+        overview.append(i["Question"], i['Answer'], i['Success'],i['Fails'])
     print("Exiting the program...")
 
-    overview = file[["Question","Success","Fails"]]
+    
 
     print(overview)
 
-    #break
-
+    
+    file.close()
     return overview #returns the overview of the questions, success and fails
 
 
 
 def add(question,answer):#to add a new question and answer
 
-    global file
+    file= open("info.xlsx",'a', newline='')
+    
+    wo= csv.writer(file)
+
     
     if question =="" or answer =="": #checks incase question and answer fields are empty
         raise(Empty)
 
     else:
-        new= {"Question":question,"Answer":answer,"Success":0,"Fails":0} #answer can only be a single word
-        new_df = pd.DataFrame([new])
-        file = pd.concat([file, new_df], ignore_index=True) #adds the new question and answer to the file
-        file.to_excel("info.xlsx", index=False)
+        new= [question.lower().strip(),answer,0,0] #answer can only be a single word
+        wo.writerow(new)
+
+    file.close()
 
 
 def check(question, answer):
-    correct_answer = file[file["Question"] == question]["Answer"].iloc[0]
+    file= open("info.xlsx",'r', newline='')
 
-    if answer.lower().strip() == correct_answer.lower().strip():
-        print("Correct!")
-        file.loc[file["Question"] == question, "Success"] = file[file["Question"] == question]["Success"].iloc[0] + 1  
-        return True, correct_answer  
-    else:
-        print(f"Incorrect! The correct answer is: {correct_answer}")
-        file.loc[file["Question"] == question, "Fails"] = file[file["Question"] == question]["Fails"].iloc[0] + 1
-        return False, correct_answer  
+    ro = csv.DictReader(file)
+
+    for i in ro:
+        if question== i["Question"]:
+            if answer.lower().strip() == i["Answer"]:
+                print("Correct!")
+                return True, i["Answer"]
+            else:
+                print(f"Incorrect! The correct answer is: {i['Answer']}")
+                return False, i["Answer"]
+
+    file.close()
 
 
 def show(): #shows a random question from the file and asks the answer
     
-    global file
+    file= open("info.xlsx",'r', newline='')
+    ro = csv.DictReader(file)
 
-    questions = file["Question"].tolist() #gets all the questions from the file
+    questions=[]
+    for i in ro:
+        questions.append(ro["Question"]) #gets all the questions from the file
     
     if questions==[]: #checks if the file is empty
         raise(Empty) #if empty the empty exception is raised 
@@ -77,22 +92,32 @@ def show(): #shows a random question from the file and asks the answer
         questions.remove(question) #removes question from list after asking it once
         
         check(question,answer)
-    
+    file.close()
     
 def edit(question, new_question, new_answer): #to edit a question and answer
-    global file
-
-    questions = file["Question"].tolist() #gets all the questions from the file
-
-    if question not in questions: #checks if the question is in the file
+    file= open("info.xlsx",'r', newline='')
+    questions=[]
+    ro = csv.DictReader(file)
+    rows=list(ro)
+    for i in rows:
+        questions.append(rows["Question"]) #gets all the questions from the file#gets all the questions from the file
+    file.close()
+    if question.lower().strip() not in questions: #checks if the question is in the file
         print(f"Question '{question}' not found!") 
         return
     
+    
     else: #replaces old question and answers with new ones and resets the success and fails count
-        file.loc[file["Question"] == question, "Answer"] = new_answer 
-        file.loc[file["Question"] == question, "Question"] = new_question
-        file.loc[file["Question"] == question, "Success"] = 0
-        file.loc[file["Question"] == question, "Fails"] = 0
-
-
-        file.to_excel("info.xlsx", index=False)
+        file= open("info.xlsx",'w', newline='')
+        fieldnames = ['Question','Answer' 'Success', 'Fails']
+        for i in rows:
+            if i["Question"]== question.lower().strip():
+                i["Question"]=new_question
+                i["Answer"]=new_answer
+                i["Success"]=0
+                i["Fails"]=0
+        wo = csv.DictWriter(file, fieldnames=fieldnames)
+        wo.writeheader()
+        wo.write(rows)
+        file.close()
+    
